5109d3bacc02160f369ad58c69008ab3fc069f8a
Navishka Kularathna <navishka.randika@gmail.com>
2020-04-29 16:45:51 +0530

    Use of Dependency Injection to get a fortune

--- src/com/navishka/springdemo/BaseballCoach.java
+++ src/com/navishka/springdemo/BaseballCoach.java
@@ -2,6 +2,12 @@ package com.navishka.springdemo;
 
 public class BaseballCoach implements Coach{
 	
+	private FortuneService fortuneService;
+	
+	public BaseballCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Bat for 30 minutes";
@@ -9,8 +15,7 @@ public class BaseballCoach implements Coach{
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return fortuneService.getFortune();
 	}
 
 }
