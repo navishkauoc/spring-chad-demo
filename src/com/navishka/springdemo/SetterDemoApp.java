eeb7601451c5bd10a077c05ce6cc7b485647c2c0
Navishka Kularathna <navishka.randika@gmail.com>
2020-05-01 16:44:37 +0530

    New class created with main method; diagnostic messages are working

---
+++ src/com/navishka/springdemo/SetterDemoApp.java
@@ -0,0 +1,20 @@
+package com.navishka.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Coach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+		
+//		System.out.println(theCoach.getDailyWorkout());
+//		
+//		System.out.println(theCoach.getDailyFortune());
+		
+		context.close();
+
+	}
+
+}
