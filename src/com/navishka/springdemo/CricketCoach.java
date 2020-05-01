ea2828d025110237013c84674cb04fc4ab62541d
Navishka Kularathna <navishka.randika@gmail.com>
2020-05-01 16:07:47 +0530

    Cricket coach class - setter method created for DI

--- src/com/navishka/springdemo/CricketCoach.java
+++ src/com/navishka/springdemo/CricketCoach.java
@@ -5,9 +5,14 @@ public class CricketCoach implements Coach{
 	private FortuneService fortuneService;
 	
 	public CricketCoach() {
-		
+		System.out.println("CricketCoach: Inside no-arg constructor");
 	}
 	
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("CricketCoach: Inside setter method - setFortuneService");
+		this.fortuneService = fortuneService;
+	}
+
 	@Override
 	public String getDailyWorkout() {
 		return "Ball for 15mins";
