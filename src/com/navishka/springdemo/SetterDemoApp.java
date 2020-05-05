d93891241eae99b64c85bed7705d315b1d898606
Navishka Kularathna <navishka.randika@gmail.com>
2020-05-05 23:22:11 +0530

    SetterDemoApp.java updated  - calling the new methods

--- src/com/navishka/springdemo/SetterDemoApp.java
+++ src/com/navishka/springdemo/SetterDemoApp.java
@@ -7,11 +7,15 @@ public class SetterDemoApp {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		Coach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		
 		System.out.println(theCoach.getDailyWorkout());
 		
 		System.out.println(theCoach.getDailyFortune());
+		
+		System.out.println(theCoach.getEmailAddress());
+		
+		System.out.println(theCoach.getTeam());
 		
 		context.close();
 
