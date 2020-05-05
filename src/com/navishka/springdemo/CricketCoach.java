50d4f03ef9b3fd54e91886020666d72c871c43c6
Navishka Kularathna <navishka.randika@gmail.com>
2020-05-05 20:18:10 +0530

    Injecting literal values

--- src/com/navishka/springdemo/CricketCoach.java
+++ src/com/navishka/springdemo/CricketCoach.java
@@ -4,10 +4,31 @@ public class CricketCoach implements Coach{
 
 	private FortuneService fortuneService;
 	
+	private String emailAddress;
+	private String team;
+	
 	public CricketCoach() {
 		System.out.println("CricketCoach: Inside no-arg constructor");
 	}
 	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: Inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: Inside setter method - setTeam");
+		this.team = team;
+	}
+
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("CricketCoach: Inside setter method - setFortuneService");
 		this.fortuneService = fortuneService;
